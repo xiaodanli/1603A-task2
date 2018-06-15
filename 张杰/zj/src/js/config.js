33c320b2e0f3bd4679a74098e4f042d8c4d7fe23
zj <940233351@qq.com>
2018-06-15 09:50:00 +0800

    sc

--- 张杰/zj/src/js/config.js
+++
@@ -1,8 +0,0 @@
-require.config({
-    baseUrl: './js',
-    paths: {
-        "jquery": "../libs/jquery.min",
-        "handlebars": "../libs/handlebars-v4.0.11",
-        "template": "../libs/template"
-    }
-})
